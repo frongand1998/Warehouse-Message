053487d36d04cbf3d01f81d6fd9226a813984af3
Chirasin Chuenthed <chirasin.chu+skyaidev@skyai.co.th>
2025-12-06 23:51:20 +0700

    Fix electron build configuration and update start URL path

--- client/public/electron.js
+++ client/public/electron.js
@@ -30,7 +30,7 @@ function createWindow() {
 
   const startURL = isDev
     ? "http://localhost:3000"
-    : `file://${path.join(__dirname, "build/index.html")}`;
+    : `file://${path.join(__dirname, "..", "build", "index.html")}`;
 
   mainWindow.loadURL(startURL);
 
